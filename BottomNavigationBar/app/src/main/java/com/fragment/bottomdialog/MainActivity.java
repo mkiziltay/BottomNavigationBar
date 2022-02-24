4ec92f25b02c3a8e6755530554547c8bd9f32f08
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-02-24 15:53:46 +0300

    Add files via upload

---
+++ BottomNavigationBar/app/src/main/java/com/fragment/bottomdialog/MainActivity.java
@@ -0,0 +1,73 @@
+package com.fragment.bottomdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int  ID_Home = 1;
+    private final int   ID_Message = 2;
+    private final int  ID_Notification = 3;
+    private final int  ID_Account = 4;
+
+    TextView selected_page;
+    MeowBottomNavigation bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        selected_page = findViewById(R.id.selPage);
+        bottomNavigation = findViewById(R.id.bottomNav);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_Home,R.drawable.home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_Message,R.drawable.message_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_Notification,R.drawable.notify_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_Account,R.drawable.account_24));
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                // Toast message : when clicked.
+                Toast.makeText(getApplicationContext(),"clicked item : "+item.getId(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                String name;
+                switch (item.getId()){
+
+                    case ID_Home: name="Home";
+                    break;
+
+                    case ID_Message: name="Message";
+                    break;
+
+                    case ID_Notification: name="Notification";
+                    break;
+
+                    case ID_Account: name="Account";
+                    break;
+
+                    default: name="";
+                }
+                //set TextView with source name.
+                selected_page.setText(name+" page is selected");
+            }
+        });
+
+        // will show 4 to top of ID_Notification Icon
+        bottomNavigation.setCount(ID_Notification,"4");
+        // set default page when start
+        bottomNavigation.show(ID_Account,true);
+
+    }
+}
